1b20c6354aa38c79e1c10fb1c3d9ab470a8ce51f
Jivak Sute <jivakysute@gmail.com>
2025-11-11 11:26:41 +0530

    swap two nuber without extra veriable

---
+++ swap.js
@@ -0,0 +1,23 @@
+// Sorting is change array into string and compare values by there UTF-16 values
+// so always add compareFN with sortFN;
+// const arr = [3,10,7,20];
+// console.log(arr.sort((a,b)=>a-b));
+
+// Swap veriables without using extra veriable
+let a = 5;
+let b = 10;
+
+// TECH 1
+
+a = a + b;
+15;
+b = a - b;
+5;
+a = a - b;
+10;
+console.log("a =", a);
+console.log("b =", b)[
+  // TECH 2
+  (a, b)
+] = [b, a];
+// console.log(a, b);
