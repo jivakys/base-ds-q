b64ab0f8ad789b3efcbbb5dc6f7b57d8302f183e
Jivak Sute <jivakysute@gmail.com>
2025-11-18 13:06:03 +0530

    color question for brute force.

--- color.js
+++ color.js
@@ -3,7 +3,7 @@ const colors = ["red", "green", "blue"];
 for (let i = 0; i < colors.length; i++) {
   for (let j = 0; j < colors.length; j++) {
     for (let k = 0; k < colors.length; k++) {
-      if (i !== j && j !== k && i !== k) {
+      if (i != j && j != k && i != k) {
         console.log(colors[i], colors[j], colors[k]);
       }
     }
