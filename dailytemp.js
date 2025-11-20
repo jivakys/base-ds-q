e16d1341b00f251b81ee2c79b033475c90e46f73
Jivak Sute <jivakysute@gmail.com>
2025-11-20 10:54:30 +0530

    stack daily temparature que

---
+++ dailytemp.js
@@ -0,0 +1,20 @@
+let array = [73, 74, 75, 71, 69, 72, 76, 73];
+let n = 8;
+
+function dail(arr, n) {
+  let stack = [];
+  let ans = [];
+  for (let i = n - 1; i >= 0; i--) {
+    while (stack.length != 0 && arr[stack[stack.length - 1]] <= arr[i]) {
+      stack.pop();
+    }
+    if (stack.length == 0) {
+      ans.push(0);
+    } else {
+      ans.push(stack[stack.length - 1] - i);
+    }
+    stack.push(i);
+  }
+  console.log(ans.reverse().join(" "));
+}
+dail(array, n);
