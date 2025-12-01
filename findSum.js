8060a0dfa283574d3f5a2858d51ba49a12a3af2c
Jivak Sute <jivakysute@gmail.com>
2025-12-01 14:54:02 +0530

    find sum of two int in array

--- findSum.js
+++ findSum.js
@@ -2,3 +2,20 @@
 // and k of 17, return true since 10 + 7 is 17;
 
 //### Solution - 1 - nested loop
+
+//### solution - 2 - using sort() and two pointer
+// function findSum(arr, k) {
+//   arr.sort((a, b) => a - b);
+//   let left = 0;
+//   let right = arr.length - 1;
+//   while (left < right) {
+//     if (arr[left] + arr[right] == k) {
+//       return true;
+//     } else if (arr[left] + arr[right] > k) {
+//       right--;
+//     } else {
+//       left++;
+//     }
+//   }
+//   return false;
+// }
