508eb9be9f2e1900a21f7cda5bc85ee037cfc169
Jivak Sute <jivakysute@gmail.com>
2025-12-01 14:49:50 +0530

    find sum of two int in array

---
+++ findSum.js
@@ -0,0 +1,4 @@
+// Given a list of a number k, return any two numbers from the list and add up give [10, 15, 3, 7]
+// and k of 17, return true since 10 + 7 is 17;
+
+//### Solution - 1 - nested loop
