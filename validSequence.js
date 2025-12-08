9fd60163b3c66e6abc05a7b96404ad3af55c08e9
Jivak Sute <jivakysute@gmail.com>
2025-12-08 12:13:13 +0530

    valid stack sequence

--- validSequence.js
+++ validSequence.js
@@ -6,4 +6,23 @@ let arr2 = [4, 5, 3, 2, 1];
 // 1 2 3 4 5
 // 4 3 5 1 2
 
-function validSequence(n, arr1, arr2) {}
+function validSequence(n, arr1, arr2) {
+  let count = 0;
+  let stack = [];
+  for (let i = 0; i < n; i++) {
+    stack.push(arr1[i]);
+    while (
+      stack.length != 0 &&
+      count < n &&
+      stack[stack.length - 1] == arr2[count]
+    ) {
+      stack.pop();
+      count++;
+    }
+  }
+  if (stack.length === 0) {
+    console.log("Valid");
+  } else {
+    console.log("Invalid");
+  }
+}
