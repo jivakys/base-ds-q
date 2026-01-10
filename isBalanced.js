f9255646d5ba03aed66c4bdde7e01ff9384e9914
Jivak Sute <jivakysute@gmail.com>
2026-01-10 18:20:14 +0530

    stack ({[]}) is Balanced question

--- isBalanced.js
+++ isBalanced.js
@@ -1,25 +1,24 @@
-function isBalanced(s) {
-  let st = [];
+// function isBalanced(s) {
+//   let st = [];
 
-  for (let c of s) {
-    if (c === "(" || c === "{" || c === "[") {
-      st.push(c);
-    } else if (c === ")" || c === "}" || c === "]") {
-      if (st.length === 0) return false;
-      let top = st[st.length - 1];
-      if (
-        (c === ")" && top !== "(") ||
-        (c === "}" && top !== "{") ||
-        (c === "]" && top !== "[")
-      ) {
-        return false;
-      }
-      st.pop();
-    }
-  }
+//   for (let c of s) {
+//     if (c === "(" || c === "{" || c === "[") {
+//       st.push(c);
+//     } else if (c === ")" || c === "}" || c === "]") {
+//       if (st.length === 0) return false;
+//       let top = st[st.length - 1];
+//       if (
+//         (c === ")" && top !== "(") ||
+//         (c === "}" && top !== "{") ||
+//         (c === "]" && top !== "[")
+//       ) {
+//         return false;
+//       }
+//       st.pop();
+//     }
+//   }
+//   return st.length === 0;
+// }
 
-  return st.length === 0;
-}
-
-let s = "[()()]{}";
+let s = "[()()]{}}{";
 console.log(isBalanced(s) ? "true" : "false");
