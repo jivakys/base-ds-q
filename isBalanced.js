a9c114d24cee132ea3b8c732a9427e5071dcd429
Jivak Sute <jivakysute@gmail.com>
2026-01-10 18:20:38 +0530

    stack ({[]}) is Balanced question

--- isBalanced.js
+++ isBalanced.js
@@ -20,5 +20,30 @@
 //   return st.length === 0;
 // }
 
+function isBalanced(s) {
+  let obj = {
+    ")": "(",
+    "]": "[",
+    "}": "{",
+  };
+
+  let stack = [];
+
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == "(" || s[i] == "[" || s[i] == "{") {
+      stack.push(s[i]);
+    } else {
+      if (stack.length == 0) {
+        return false;
+      } else if (stack[stack.length - 1] != obj[s[i]]) {
+        return false;
+      } else {
+        stack.pop();
+      }
+    }
+  }
+  return stack.length == 0;
+}
+
 let s = "[()()]{}}{";
 console.log(isBalanced(s) ? "true" : "false");
