d2c85b6766cf79f06ece9e2705794fad109e12bf
Jivak Sute <jivakysute@gmail.com>
2026-01-27 10:41:35 +0530

    flatten nested arr with concat

--- flattenArr.js
+++ flattenArr.js
@@ -1,13 +1,13 @@
-// const multiDimensionalArray = [
-//   [1, 2, 3],
-//   [[4, 5], 6],
-//   [[7, 8], 9],
-// ];
 const multiDimensionalArray = [
   [1, 2, 3],
-  [4, 5, 6],
-  [7, 8, 9],
+  [[4, 5], 6],
+  [[7, 8], 9],
 ];
+// const multiDimensionalArray = [
+//   [1, 2, 3],
+//   [4, 5, 6],
+//   [7, 8, 9],
+// ];
 // const flattenedArray = multiDimensionalArray.reduce((acc, currval) => {
 //   return acc.concat(currval);
 // }, []);
